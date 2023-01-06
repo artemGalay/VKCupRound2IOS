4d67daead466a6740a92f64797b14476bc0039f2
Artem Galay <iostema@mail.ru>
2023-01-06 23:52:05 +0300

    Initial Commit

---
+++ VKCupRound2IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKCupRound2IOS
+//
+//  Created by Артем Галай on 6.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
